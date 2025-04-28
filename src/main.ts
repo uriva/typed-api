a943607fbace7a66a51db4969afd985c2bc049e7
uri <uri.valevski@gmail.com>
2025-04-29 00:12:33 +0300

    deploy

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+type Endpoint<Input, Output> = { input: Input; output: Output };
+
+type ApiDefinition = Record<string, Endpoint<unknown, unknown>>;
+
+type Handler<User, EPs extends ApiDefinition, E extends keyof EPs> = (
+  user: User,
+  payload: EPs[E]["input"],
+) => Promise<EPs[E]["output"]>;
+
+export type PortalApiImplementation<User, EPs extends ApiDefinition> = {
+  [E in keyof EPs]: Handler<User, EPs, E>;
+};
+
+export const portalClient =
+  <EPs extends ApiDefinition>(serverUrl: string) =>
+  async <E extends keyof EPs>(
+    endpoint: E,
+    token: string,
+    payload: EPs[E]["input"],
+  ): Promise<EPs[E]["output"]> => {
+    const response = await fetch(serverUrl, {
+      method: "POST",
+      body: JSON.stringify({ payload, endpoint, token }),
+    });
+    if (response.status !== 200) {
+      console.error(response.status);
+      return Promise.reject(await response.text());
+    }
+    return await response.json();
+  };
+
+export const PortalHandler = async <
+  User,
+  EPs extends ApiDefinition,
+  Key extends keyof EPs,
+>(
+  endpoints: PortalApiImplementation<User, EPs>,
+  verifyToken: (token: string) => Promise<User>,
+  { token, payload, endpoint }: {
+    endpoint: Key;
+    token: string;
+    payload: EPs[Key]["input"];
+  },
+) => {
+  const activeUser = await verifyToken(token);
+  const ep = endpoints[endpoint];
+  if (!ep) throw new Error("No endpoint found");
+  return ep(activeUser, payload);
+};
